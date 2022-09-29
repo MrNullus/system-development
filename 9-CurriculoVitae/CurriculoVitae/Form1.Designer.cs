1571eff820325822c1cf4cbaa90afe6d6f8c8fbb
Mr. Nullus <70487766+MrNullus@users.noreply.github.com>
2022-09-29 18:59:25 -0300

    updates project curriculo vitae

--- 9-CurriculoVitae/CurriculoVitae/Form1.Designer.cs
+++ 9-CurriculoVitae/CurriculoVitae/Form1.Designer.cs
@@ -32,14 +32,19 @@ namespace CurriculoVitae
             this.label1 = new System.Windows.Forms.Label();
             this.txtCpf = new System.Windows.Forms.TextBox();
             this.btnCadastrar = new System.Windows.Forms.Button();
-            this.lblMensagem = new System.Windows.Forms.Label();
+            this.lblMsgCpf = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEnder = new System.Windows.Forms.TextBox();
+            this.lblMsgTelefone = new System.Windows.Forms.Label();
+            this.lblMsgEnder = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(109, 57);
+            this.label1.Location = new System.Drawing.Point(109, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 13);
             this.label1.TabIndex = 0;
@@ -54,38 +59,83 @@ namespace CurriculoVitae
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(112, 130);
+            this.btnCadastrar.Location = new System.Drawing.Point(112, 308);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(156, 23);
             this.btnCadastrar.TabIndex = 2;
-            this.btnCadastrar.Text = "button1";
+            this.btnCadastrar.Text = "Cadastrar";
             this.btnCadastrar.UseVisualStyleBackColor = true;
             this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
-            // lblMensagem
+            // lblMsgCpf
             // 
-            this.lblMensagem.AutoSize = true;
-            this.lblMensagem.Location = new System.Drawing.Point(109, 322);
-            this.lblMensagem.Name = "lblMensagem";
-            this.lblMensagem.Size = new System.Drawing.Size(0, 13);
-            this.lblMensagem.TabIndex = 3;
+            this.lblMsgCpf.AutoSize = true;
+            this.lblMsgCpf.Location = new System.Drawing.Point(116, 136);
+            this.lblMsgCpf.Name = "lblMsgCpf";
+            this.lblMsgCpf.Size = new System.Drawing.Size(0, 13);
+            this.lblMsgCpf.TabIndex = 4;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(112, 165);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(156, 20);
+            this.txtTelefone.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(290, 395);
+            this.label2.Location = new System.Drawing.Point(109, 149);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Telefone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Endereço";
+            // 
+            // txtEnder
+            // 
+            this.txtEnder.Location = new System.Drawing.Point(112, 243);
+            this.txtEnder.Multiline = true;
+            this.txtEnder.Name = "txtEnder";
+            this.txtEnder.Size = new System.Drawing.Size(156, 29);
+            this.txtEnder.TabIndex = 8;
+            // 
+            // lblMsgTelefone
+            // 
+            this.lblMsgTelefone.AutoSize = true;
+            this.lblMsgTelefone.Location = new System.Drawing.Point(116, 188);
+            this.lblMsgTelefone.Name = "lblMsgTelefone";
+            this.lblMsgTelefone.Size = new System.Drawing.Size(0, 13);
+            this.lblMsgTelefone.TabIndex = 9;
+            // 
+            // lblMsgEnder
+            // 
+            this.lblMsgEnder.AutoSize = true;
+            this.lblMsgEnder.Location = new System.Drawing.Point(116, 275);
+            this.lblMsgEnder.Name = "lblMsgEnder";
+            this.lblMsgEnder.Size = new System.Drawing.Size(0, 13);
+            this.lblMsgEnder.TabIndex = 10;
             // 
             // frmCurriculo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1110, 595);
+            this.Controls.Add(this.lblMsgEnder);
+            this.Controls.Add(this.lblMsgTelefone);
+            this.Controls.Add(this.txtEnder);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblMsgCpf);
             this.Controls.Add(this.btnCadastrar);
             this.Controls.Add(this.txtCpf);
             this.Controls.Add(this.label1);
@@ -101,8 +151,13 @@ namespace CurriculoVitae
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtCpf;
         private System.Windows.Forms.Button btnCadastrar;
-        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Label lblMsgCpf;
+        private System.Windows.Forms.TextBox txtTelefone;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEnder;
+        private System.Windows.Forms.Label lblMsgTelefone;
+        private System.Windows.Forms.Label lblMsgEnder;
     }
 }
 
--- 9-CurriculoVitae/CurriculoVitae/Form1.cs
+++ 9-CurriculoVitae/CurriculoVitae/Form1.cs
@@ -20,6 +20,10 @@ namespace CurriculoVitae
 
         private void btnCadastrar_Click(object sender, EventArgs e)
         {
+            setarLabel(lblMsgCpf, "");
+            setarLabel(lblMsgTelefone, "");
+            setarLabel(lblMsgEnder, "");
+
             // dados iniciais
             List<int> onzeDigitos = new List<int>();
             List<int> noveDigitos = new List<int>();
@@ -28,60 +32,90 @@ namespace CurriculoVitae
             bool cpfNaoNumerico = false;
             string cpfDigitado = txtCpf.Text;
 
+
+            // verificação primaria - (verificar quantidade de digitos informados e se estão vazios)
             int qtdeDigitosDoCpf = txtCpf.TextLength;
-            if (qtdeDigitosDoCpf != 11)
+            int qtdeDigitosDoTel = txtTelefone.TextLength;
+
+            if (estaVazio(txtCpf) && estaVazio(txtTelefone) && estaVazio(txtEnder)) 
             {
-                MessageBox.Show("O campo CPF deve conter todos os 11 digitos!");
+                setarLabel(lblMsgCpf, "Campo obrigatório!");
+                setarLabel(lblMsgTelefone, "Campo obrigatório!");
+                setarLabel(lblMsgEnder, "Campo obrigatório!");
+
                 return;
             }
+
+            if (qtdeDigitosDoCpf != 11 && qtdeDigitosDoTel != 12)
+            {
+               setarLabel(lblMsgCpf, "O campo CPF deve conter todos os 11 digitos!");
+               setarLabel(lblMsgTelefone, "Telefone está invalido!");
+
+               return;
+            }
+
 
             // pegar os nove digitos do cpf digitado
             for (int posicao = 0; posicao < cpfDigitado.Length; posicao++)
             {
                 string carac = cpfDigitado.Substring(posicao, 1);
+
                 if (int.TryParse(carac, out int digito))
                 {
-                    lblMensagem.Text += " " + digito;
-
                     if (posicao <= 8)
                     {
                         noveDigitos.Add(digito);
-                    } else
+                    } 
+                    else
                     {
                         ultimos2Digitos += digito.ToString();
                     }
-                } else
+                } 
+                else
                 {
                     cpfNaoNumerico = true;
                 }
                 
             }
+
+
+            // verificações secundarias - (se possui apenas numeros e se é valido)
             if (cpfNaoNumerico)
             {
-                MessageBox.Show("Informe apenas os onze digitos do CPF!");
-                return;
+               setarLabel(lblMsgCpf, "Informe apenas os onze digitos do CPF!");
+               return;
             }
-
             if (verificarCpf(noveDigitos, ultimos2Digitos))
             {
                 MessageBox.Show("Curriculo cadastrado!");
             }
-
-
         }
 
+
+        // função que irá verificar se o CPF informado é valido ou não     
         private bool verificarCpf(List<int> noveDigitos, string ultimos2Digitos)
         {
-            // digito 10 e 11
+            // digito 10 e 11 calculados
             int digito10 = calcDigito10(noveDigitos);
             int digito11 = calcDigito11(noveDigitos, digito10);
-            string ultimos2Digitos = string.Empty;
-            string ultimosDigiCalculados = string.Empty;
+
+            // tornando os digitos 10 e 11 (inseridos e calculados) em um unico texto 
+            string ultimosDigitos = ultimos2Digitos[0].ToString() + ultimos2Digitos[1].ToString();
+            string ultimosDigiCalculados = digito10.ToString() + digito11.ToString();
 
             // verificar se esses digitos são correspondentes
             bool cpfValido = validarDoisUltimosDigitos(ultimos2Digitos, ultimosDigiCalculados);
 
-            return true;
+            // autenticação final - verificar se é ou não valido
+            if (cpfValido) 
+            {
+                return true;
+            }
+            else
+            {
+                setarLabel(lblMsgCpf, "CPF invalido! Por favor, insira um CPF valido");
+                return false;
+            }
         }
 
 
@@ -114,7 +148,7 @@ namespace CurriculoVitae
         }
 
 
-        // funcções de calculos dos digito 10 e 11
+        // funções de calculos dos digito 10 e 11
         private int calcDigito10(List<int> noveDigitos)
         {
             int digito10 = 0, resto = 0;
@@ -137,12 +171,33 @@ namespace CurriculoVitae
         }
 
 
-        // verificar 
-        private bool ValidarDoisUltimosDigitos(string ultimos2Digitos, string ultimosDigiCalculados)
+        // validara se os ultimos digitos inseridos são correspondentes aos calculados 
+        private bool validarDoisUltimosDigitos(string ultimos2Digitos, string ultimosDigitosCalculados)
         {
-            
+            if (ultimos2Digitos == ultimosDigitosCalculados)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-            return true; 
+
+        // funções auxiliares
+        private bool estaVazio(TextBox campo) {
+            if (campo.Text == string.Empty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void setarLabel(Label lbl, string msg) {
+            lbl.Text = msg;
         }
     }
 }
