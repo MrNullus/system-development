d87658e97f38239a17c914227f3d2b6a365914f6
Mr. Nullus <70487766+MrNullus@users.noreply.github.com>
2022-09-30 18:59:32 -0300

    updates in design

--- 9-CurriculoVitae/CurriculoVitae/Form1.Designer.cs
+++ 9-CurriculoVitae/CurriculoVitae/Form1.Designer.cs
@@ -44,13 +44,21 @@ namespace CurriculoVitae
             this.txtNome = new System.Windows.Forms.TextBox();
             this.lblMsgNome = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(447, 241);
+            this.label1.ForeColor = System.Drawing.Color.MediumAquamarine;
+            this.label1.Location = new System.Drawing.Point(80, 250);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 13);
             this.label1.TabIndex = 0;
@@ -58,32 +66,35 @@ namespace CurriculoVitae
             // 
             // txtCpf
             // 
-            this.txtCpf.Location = new System.Drawing.Point(450, 257);
+            this.txtCpf.Location = new System.Drawing.Point(83, 268);
             this.txtCpf.Name = "txtCpf";
             this.txtCpf.Size = new System.Drawing.Size(156, 20);
             this.txtCpf.TabIndex = 1;
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(450, 461);
+            this.btnCadastrar.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.ForeColor = System.Drawing.Color.Aquamarine;
+            this.btnCadastrar.Location = new System.Drawing.Point(83, 470);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(156, 40);
             this.btnCadastrar.TabIndex = 2;
             this.btnCadastrar.Text = "Cadastrar";
-            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.UseVisualStyleBackColor = false;
             this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
             // lblMsgCpf
             // 
             this.lblMsgCpf.AutoSize = true;
-            this.lblMsgCpf.Location = new System.Drawing.Point(454, 290);
+            this.lblMsgCpf.Location = new System.Drawing.Point(87, 299);
             this.lblMsgCpf.Name = "lblMsgCpf";
             this.lblMsgCpf.Size = new System.Drawing.Size(0, 13);
             this.lblMsgCpf.TabIndex = 4;
             // 
             // txtTelefone
             // 
-            this.txtTelefone.Location = new System.Drawing.Point(450, 335);
+            this.txtTelefone.Location = new System.Drawing.Point(83, 346);
             this.txtTelefone.Name = "txtTelefone";
             this.txtTelefone.Size = new System.Drawing.Size(156, 20);
             this.txtTelefone.TabIndex = 5;
@@ -91,7 +102,8 @@ namespace CurriculoVitae
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(447, 319);
+            this.label2.ForeColor = System.Drawing.Color.MediumAquamarine;
+            this.label2.Location = new System.Drawing.Point(80, 328);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 6;
@@ -100,7 +112,8 @@ namespace CurriculoVitae
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(450, 396);
+            this.label3.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.label3.Location = new System.Drawing.Point(83, 405);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 7;
@@ -108,7 +121,7 @@ namespace CurriculoVitae
             // 
             // txtEnder
             // 
-            this.txtEnder.Location = new System.Drawing.Point(450, 413);
+            this.txtEnder.Location = new System.Drawing.Point(83, 422);
             this.txtEnder.Multiline = true;
             this.txtEnder.Name = "txtEnder";
             this.txtEnder.Size = new System.Drawing.Size(156, 29);
@@ -117,7 +130,7 @@ namespace CurriculoVitae
             // lblMsgTelefone
             // 
             this.lblMsgTelefone.AutoSize = true;
-            this.lblMsgTelefone.Location = new System.Drawing.Point(454, 358);
+            this.lblMsgTelefone.Location = new System.Drawing.Point(87, 367);
             this.lblMsgTelefone.Name = "lblMsgTelefone";
             this.lblMsgTelefone.Size = new System.Drawing.Size(0, 13);
             this.lblMsgTelefone.TabIndex = 9;
@@ -125,7 +138,7 @@ namespace CurriculoVitae
             // lblMsgEnder
             // 
             this.lblMsgEnder.AutoSize = true;
-            this.lblMsgEnder.Location = new System.Drawing.Point(454, 445);
+            this.lblMsgEnder.Location = new System.Drawing.Point(87, 454);
             this.lblMsgEnder.Name = "lblMsgEnder";
             this.lblMsgEnder.Size = new System.Drawing.Size(0, 13);
             this.lblMsgEnder.TabIndex = 10;
@@ -133,7 +146,9 @@ namespace CurriculoVitae
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(447, 172);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.MediumAquamarine;
+            this.label4.Location = new System.Drawing.Point(80, 181);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 11;
@@ -141,7 +156,7 @@ namespace CurriculoVitae
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(450, 188);
+            this.txtNome.Location = new System.Drawing.Point(83, 200);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(156, 20);
             this.txtNome.TabIndex = 12;
@@ -149,26 +164,120 @@ namespace CurriculoVitae
             // lblMsgNome
             // 
             this.lblMsgNome.AutoSize = true;
-            this.lblMsgNome.Location = new System.Drawing.Point(450, 211);
+            this.lblMsgNome.Location = new System.Drawing.Point(83, 220);
             this.lblMsgNome.Name = "lblMsgNome";
             this.lblMsgNome.Size = new System.Drawing.Size(0, 13);
             this.lblMsgNome.TabIndex = 13;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(457, 26);
+            this.pictureBox1.Location = new System.Drawing.Point(90, 35);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(132, 128);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.MintCream;
+            this.radioButton1.Location = new System.Drawing.Point(291, 200);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 17);
+            this.radioButton1.TabIndex = 15;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Analfabeto";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.MintCream;
+            this.radioButton2.Location = new System.Drawing.Point(291, 223);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(176, 17);
+            this.radioButton2.TabIndex = 16;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ensino Fundamental Incompleto";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.ForeColor = System.Drawing.Color.MintCream;
+            this.radioButton3.Location = new System.Drawing.Point(291, 246);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(168, 17);
+            this.radioButton3.TabIndex = 17;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Ensino Fundamental Completo";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.ForeColor = System.Drawing.Color.MintCream;
+            this.radioButton4.Location = new System.Drawing.Point(291, 269);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(144, 17);
+            this.radioButton4.TabIndex = 18;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Ensino Medio Incompleto";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.ForeColor = System.Drawing.Color.MintCream;
+            this.radioButton5.Location = new System.Drawing.Point(291, 292);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(136, 17);
+            this.radioButton5.TabIndex = 19;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Ensino Medio Completo";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.MediumAquamarine;
+            this.label5.Location = new System.Drawing.Point(291, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Escolaridade";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.ForeColor = System.Drawing.Color.MintCream;
+            this.radioButton6.Location = new System.Drawing.Point(291, 315);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(99, 17);
+            this.radioButton6.TabIndex = 21;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Ensino Superior";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
             // frmCurriculo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1110, 595);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(759, 595);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblMsgNome);
             this.Controls.Add(this.txtNome);
@@ -183,6 +292,8 @@ namespace CurriculoVitae
             this.Controls.Add(this.btnCadastrar);
             this.Controls.Add(this.txtCpf);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmCurriculo";
             this.Text = "Curriculo Vitae";
             this.Load += new System.EventHandler(this.frmCurriculo_Load);
@@ -208,6 +319,13 @@ namespace CurriculoVitae
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Label lblMsgNome;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton6;
     }
 }
 
